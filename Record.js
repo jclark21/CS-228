cbb04368f57bf5c44228b70b5856b525fe019742
jclark21 <58433011+jclark21@users.noreply.github.com>
2020-10-05 13:05:02 -0400

    Step 26

--- Record.js
+++ Record.js
@@ -64,12 +64,12 @@ function HandleBone(bone,weight,fingerIndex,interactionBox)
     //console.log('Normalized Point',normalizedPrevJoint)
     normalizedNextJoint = interactionBox.normalizePoint(bone.nextJoint,true)
     
-    oneFrameOfData.set(fingerIndex,weight,0,normalizedPrevJoint[0])
-    oneFrameOfData.set(fingerIndex,weight,1,normalizedPrevJoint[1])
-    oneFrameOfData.set(fingerIndex,weight,2,normalizedPrevJoint[2])
-    oneFrameOfData.set(fingerIndex,weight,3,normalizedNextJoint[0])
-    oneFrameOfData.set(fingerIndex,weight,4,normalizedNextJoint[1])
-    oneFrameOfData.set(fingerIndex,weight,5,normalizedNextJoint[2])
+    framesOfData.set(fingerIndex,weight,0,currentSamples,normalizedPrevJoint[0])
+    framesOfData.set(fingerIndex,weight,1,currentSamples,normalizedPrevJoint[1])
+    framesOfData.set(fingerIndex,weight,2,currentSamples,normalizedPrevJoint[2])
+    framesOfData.set(fingerIndex,weight,3,currentSamples,normalizedNextJoint[0])
+    framesOfData.set(fingerIndex,weight,4,currentSamples,normalizedNextJoint[1])
+    framesOfData.set(fingerIndex,weight,5,currentSamples,normalizedNextJoint[2])
 
     var xb = window.innerWidth * normalizedPrevJoint[0];
     var yb = window.innerHeight * (1 - normalizedPrevJoint[1]);
@@ -112,13 +112,23 @@ function RecordData()
     if(previousNumHands == 2 && currentNumHands == 1)
     {
         background('#222222')
-        console.log(oneFrameOfData.toString());
+        console.log(framesOfData.toString());
+    }
+    if(currentNumHands == 2)
+    {
+        currentSamples++;
+        if(currentSamples == numSamples)
+        {
+            currentSamples = 0;
+        }
     }
     
 }
 
-
-var oneFrameOfData = nj.zeros([5,4,6]);
+var numSamples = 2;
+var currentSamples = 0;
+nj.config.printThreshold = 1000;
+var framesOfData = nj.zeros([5,4,6,numSamples]);
 
 var controllerOptions = {};
 var x = window.innerWidth/2;
@@ -136,6 +146,8 @@ Leap.loop(controllerOptions, function(frame)
     currentNumHands = frame.hands.length
     clear();
     HandleFrame(frame);
+    console.log( framesOfData.toString() );
+    //console.log("CUrrent Sample",currentSamples)
     RecordData();
     previousNumHands = currentNumHands
 }
